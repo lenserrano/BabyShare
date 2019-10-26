341828522c53a458a3f970cae0e8213f2dcc6878
Len Serrano <len.serrano.g@gmail.com>
2019-10-26 13:18:01 -0500

    l:dropdown fixed, likes experimentales

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,14 +1,16 @@
-function showMenu(){
-    document.getElementById("mobileMenu").classList.toggle("show");
-    console.log("entro showMenu()");
-    console.log(document.getElementById("mobileMenu").getElementsByClassName('show'));
+const c = document.getElementById('menu-mobile');
+window.addEventListener('click', toggleMenuClose);
+c.addEventListener('click', toggleMenuOpen);
+function toggleMenuOpen(e){
+    e.stopPropagation();
+        document.getElementById("mobileMenu").classList.toggle("show");
+        console.log("entro showMenu()");
+        console.log(document.getElementById("mobileMenu").getElementsByClassName('show'));
 }
-window.onclick = function(e){
-    if( !e.target.matches('.dropbtn')){
+function toggleMenuClose(){
         let dropdownMenu = document.getElementById("mobileMenu");
         if(dropdownMenu.classList.contains('show')){
             dropdownMenu.classList.remove('show');
             console.log("Me sali");
         }
-    }
 }
